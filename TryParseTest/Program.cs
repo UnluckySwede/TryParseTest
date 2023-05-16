e5ed6b7da63984dcb7dc2f07379a1b37f2a04345
UnluckySwede <gadler.michael@elev.ga.ntig.se>
2023-05-16 13:09:06 +0200

    Tryparsing

---
+++ TryParseTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*/ int points = 100;
+string name = "Micke";
+
+string t = "20";
+/*/
+// Console.Writeline(t + 90);
+
+System.Console.WriteLine(int.MaxValue);
+
+System.Console.WriteLine("Skriv in ett tal:");
+
+string tal = Console.ReadLine();
+int resultat;
+
+// resultat = int.Parse(tal);
+
+bool success = int.TryParse(tal, out resultat);
+
+if (success == true)
+{
+    System.Console.WriteLine(resultat * 2);
+}
+else
+{
+    System.Console.WriteLine("Är du dum i huvudet?");
+}
+
+// int talNum = tal;
+
+Console.ReadLine();
